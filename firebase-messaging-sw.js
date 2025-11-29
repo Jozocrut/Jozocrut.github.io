fd2b6b9e85e0eef5ba59af6bb390f55585c4bcf5
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-11-28 21:48:28 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,7 @@
-importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-compat.js");
+// firebase-messaging-sw.js
+
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
@@ -7,17 +9,17 @@ firebase.initializeApp({
     projectId: "delfos-84316",
     storageBucket: "delfos-84316.appspot.com",
     messagingSenderId: "308101271742",
-    appId: "1:308101271742:web:ee240676dcd05927cbceef",
+    appId: "1:308101271742:web:ee240676dcd05927cbceef"
 });
 
 const messaging = firebase.messaging();
 
-// Notificación cuando la app está cerrada
 messaging.onBackgroundMessage((payload) => {
-    console.log("Mensaje en background:", payload);
+    console.log("Mensaje recibido en background:", payload);
 
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
         icon: "./img/favicon_192.png"
     });
 });
+
