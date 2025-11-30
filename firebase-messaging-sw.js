25701685ab1036eac162ea20b268cce189516ca2
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-11-30 15:33:00 -0600

    Upgrade Firebase SDK and refactor background messaging

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,24 @@
-// firebase-messaging-sw.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-sw.js";
 
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
-
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
     authDomain: "delfos-84316.firebaseapp.com",
     projectId: "delfos-84316",
     storageBucket: "delfos-84316.appspot.com",
     messagingSenderId: "308101271742",
     appId: "1:308101271742:web:ee240676dcd05927cbceef"
-});
+};
 
-messaging.onBackgroundMessage((payload) => {
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+onBackgroundMessage(messaging, (payload) => {
     console.log("Mensaje recibido en background:", payload);
 
-    const notif = payload.notification || payload.data || {};
-
-    const title = notif.title || "Sin título";
-    const body  = notif.body  || "Sin mensaje";
-
-    self.registration.showNotification(title, {
-        body: body,
+    const notif = payload.notification || {};
+    self.registration.showNotification(notif.title, {
+        body: notif.body,
         icon: "./img/favicon_192.png"
     });
 });
-
-
