4c96c9b9d9a7927e5efa045daf797e9f896991bc
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-11-30 16:04:06 -0600

    Refactor background message notification handling

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
-// Tu config de Firebase
 firebase.initializeApp({
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
     authDomain: "delfos-84316.firebaseapp.com",
@@ -11,18 +10,16 @@ firebase.initializeApp({
     appId: "1:308101271742:web:ee240676dcd05927cbceef"
 });
 
-// Inicializar FCM
 const messaging = firebase.messaging();
 
-// Notificación cuando la app está cerrada
 messaging.onBackgroundMessage((payload) => {
     console.log("Mensaje en background:", payload);
 
     const title = payload.notification?.title || "Notificación";
-    const body  = payload.notification?.body  || "Tienes un mensaje nuevo";
+    const body = payload.notification?.body || "Tienes un mensaje nuevo";
 
     self.registration.showNotification(title, {
-        body: body,
+        body,
         icon: "./img/favicon_192.png"
     });
 });
