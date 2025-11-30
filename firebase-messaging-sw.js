ae2c2394a35aabfe093f03f7df3ad25c8c25a8d7
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-11-30 15:46:28 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getMessaging, onBackgroundMessage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-sw.js";
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
-const firebaseConfig = {
+// Tu config de Firebase
+firebase.initializeApp({
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
     authDomain: "delfos-84316.firebaseapp.com",
     projectId: "delfos-84316",
     storageBucket: "delfos-84316.appspot.com",
     messagingSenderId: "308101271742",
     appId: "1:308101271742:web:ee240676dcd05927cbceef"
-};
+});
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// Inicializar FCM
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, (payload) => {
-    console.log("Mensaje recibido en background:", payload);
+// Notificación cuando la app está cerrada
+messaging.onBackgroundMessage((payload) => {
+    console.log("Mensaje en background:", payload);
 
-    const notif = payload.notification || {};
-    self.registration.showNotification(notif.title, {
-        body: notif.body,
+    const title = payload.notification?.title || "Notificación";
+    const body  = payload.notification?.body  || "Tienes un mensaje nuevo";
+
+    self.registration.showNotification(title, {
+        body: body,
         icon: "./img/favicon_192.png"
     });
 });
