1b69b283cd58c2bfa634ea95c05f5c9d5942bedf
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-12-01 18:31:47 -0600

    Update background message handling in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,15 +5,18 @@ firebase.initializeApp({
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
     authDomain: "delfos-84316.firebaseapp.com",
     projectId: "delfos-84316",
+    storageBucket: "delfos-84316.appspot.com",
     messagingSenderId: "308101271742",
     appId: "1:308101271742:web:ee240676dcd05927cbceef"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(payload => {
-    self.registration.showNotification(payload.notification.title, {
-        body: payload.notification.body,
+messaging.onBackgroundMessage((payload) => {
+    const n = payload.notification || { title: "Sin título", body: "Sin mensaje" };
+
+    self.registration.showNotification(n.title, {
+        body: n.body,
         icon: "./img/favicon_192.png"
     });
 });
