5edc0a54ec2f6e5a43d2f95fcc0c21967fd07d52
Jozocrut <162380418+Jozocrut@users.noreply.github.com>
2025-12-01 18:25:02 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,21 +5,15 @@ firebase.initializeApp({
     apiKey: "AIzaSyB3b42LSiKPVx6KjCNoYy4SNFPgdAORN5g",
     authDomain: "delfos-84316.firebaseapp.com",
     projectId: "delfos-84316",
-    storageBucket: "delfos-84316.appspot.com",
     messagingSenderId: "308101271742",
     appId: "1:308101271742:web:ee240676dcd05927cbceef"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log("Mensaje en background:", payload);
-
-    const title = payload.notification?.title || "Notificación";
-    const body = payload.notification?.body || "Tienes un mensaje nuevo";
-
-    self.registration.showNotification(title, {
-        body,
+messaging.onBackgroundMessage(payload => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
         icon: "./img/favicon_192.png"
     });
 });
